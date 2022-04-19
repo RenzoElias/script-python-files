c77e45e49c106abd844658365db57495364a086a
Renzo Miguel Elias <57267514+RenzoElias@users.noreply.github.com>
2022-04-18 23:18:28 -0500

    Fixed text encode utf-8

--- application.py
+++ application.py
@@ -7,16 +7,18 @@ totalFiles = []
 with os.scandir(currentDir) as ficheros:
     ficherosMP3 = [fichero.name for fichero in ficheros if fichero.is_file() and fichero.name.endswith('.mp3')]
     totalFiles = ficherosMP3
+    # print(ficherosMP3)
 
 
 x = currentDir.split("\\")
 nameFile = str(x[len(x)-1]) + "_MP3.txt"
 
-if (len(totalFiles) > 0) :
-    file = open(nameFile, "w")
-    for name in totalFiles:
-        file.write(name + "\n") # file.write(os.linesep)
-        
+if (len(ficherosMP3) > 0) :
+    file = open(nameFile, "w", encoding="utf-8")
+    for name in ficherosMP3:
+        file.write(str(name) + "\n")
+        # file.write(os.linesep)
+
     file.close()
 else:
     file = open("NotFoundMP3.txt", "w")
